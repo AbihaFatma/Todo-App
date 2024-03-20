070e53466a193b29fd8bea435d07a5d933401b66
AbihaFatma <abihafatma12@gmail.com>
2024-03-21 00:09:49 +0530

    first commit

---
+++ src/components/WelcomeMsg.jsx
@@ -0,0 +1,5 @@
+import styles from "./Welcome.module.css";
+const WelcomeMessage = () => {
+  return <p className={styles.welcome}>Enjoy Your Day, All Task Done</p>;
+};
+export default WelcomeMessage;
